4d7eab3a2c7913c9cbb6c42fda5e836dfaa3c013
janiM <miettinenjani@gmail.com>
2022-02-08 16:15:14 +0000

    cust2

--- frr-test/pe-r1/intf.sh
+++ frr-test/pe-r1/intf.sh
@@ -7,13 +7,14 @@ ip link set dev eth0 master mgmt
 
 # dummy intfs
 ip link add dev dum0 type dummy
+ip link set dev dum0 mtu 1550
 ip link set dev dum0 up
+
 
 # cust1 vrf + vni101 vxlan
 ip link add cust1 type vrf table 101
 ip link add br101 type bridge
 ip link add vxlan101 type vxlan id 101 dev dum0 dstport 4789
-ip link set dev dum0 mtu 1550
 ip link set dev vxlan101 mtu 1500
 ip link set dev br101 mtu 9500
 ip link set dev vxlan101 master br101
@@ -24,3 +25,18 @@ ip link set dev br101 up
 ip link set dev cust1 up
 
 ip addr add 192.168.1.0/31 dev br101
+
+# cust2 crf + vni101 vxlan
+ip link add cust2 type vrf table 102
+ip link add br102 type bridge
+ip link add vxlan102 type vxlan id 102 dev dum0 dstport 4789
+ip link set dev vxlan102 mtu 1500
+ip link set dev br102 mtu 9500
+ip link set dev vxlan102 master br102
+ip link set dev eth3 master br102
+ip link set dev br102 master cust2
+ip link set dev vxlan102 up
+ip link set dev br102 up
+ip link set dev cust2 up
+
+ip addr add 192.168.2.0/31 dev br102
--- frr-test/pe-r2/intf.sh
+++ frr-test/pe-r2/intf.sh
@@ -8,13 +8,13 @@ ip link set dev eth0 master mgmt
 
 # dummy intfs
 ip link add dev dum0 type dummy
+ip link set dev dum0 mtu 1550
 ip link set dev dum0 up
 
 # vrf cust1 + vni 101 vxlan
 ip link add cust1 type vrf table 101
 ip link add br101 type bridge
 ip link add vxlan101 type vxlan id 101 dev dum0 dstport 4789
-ip link set dev dum0 mtu 1550
 ip link set dev vxlan101 mtu 1500
 ip link set dev br101 mtu 9500
 ip link set dev vxlan101 master br101
